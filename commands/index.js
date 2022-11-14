8d2b3847732e8eaaac63adfbdb558d672c267f93
8sunyuan <michaelsun97@gmail.com>
2022-11-14 10:55:13 -0500

    refactor code

---
+++ commands/index.js
@@ -0,0 +1,27 @@
+import slashCommands from "./slashCommands.js";
+import interactiveCommands from "./interactiveCommands.js";
+
+export async function HandleCommands(type, id, data) {
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    slashCommands(id, data);
+  }
+
+  /**
+   * Handle requests from interactive components
+   * See https://discord.com/developers/docs/interactions/message-components#responding-to-a-component-interaction
+   */
+  if (type === InteractionType.MESSAGE_COMPONENT) {
+    interactiveCommands(id, data);
+  }
+}

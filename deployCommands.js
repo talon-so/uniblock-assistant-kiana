5a8a39ce99f4b78d532ab71f3a0432d2a374b387
talon-so <talon_so@Hotmail.com>
2022-11-14 15:54:02 -0500

    custom deployCommands

--- deployCommands.js
+++ deployCommands.js
@@ -2,20 +2,30 @@ require("dotenv").config();
 const { REST, SlashCommandBuilder, Routes } = require("discord.js");
 
 const fs = require("fs");
+const { SocketAddress } = require('net');
 
 const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
 
 const commands = [];
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  commands.push(
-    new SlashCommandBuilder()
-      .setName(command.name)
-      .setDescription(command.description)
-  );
+  console.log(command);
+  let builder = new SlashCommandBuilder()
+    .setName(command.name)
+    .setDescription(command.description);
+  switch (command.name) {
+    case 'get_balance':
+      builder.addStringOption((option) =>
+        option.setName('address').setDescription('A valid ethereum address')
+      );
+      break;
+    default:
+      break;
+  }
+  commands.push(builder);
 }
 
 const commandsJSON = commands.map((command) => command.toJSON());

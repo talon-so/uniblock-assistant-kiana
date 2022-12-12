65f7d6405047bc71bfc4e3e9d4f5ae364827d920
8sunyuan <michaelsun97@gmail.com>
2022-12-12 18:19:04 -0500

    Discord bot status ETH price

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,15 @@
+const axios = require('axios');
+
 module.exports = {
   name: "ready",
   once: true,
   run(client) {
-    console.log("Successfully logged in as " + client.user.tag);
+    // console.log("Successfully logged in as " + client.user.tag);
+    // Update price status every 2 minutes
+    setInterval(async () => {
+      const axiosRes = await axios.get(process.env.UNIBLOCK_BASE_URL + '/price?address=0x0000000000000000000000000000000000000000&chainId=1');
+      const price = axiosRes["priceData"][0]["price"];
+      client.user.setActivity(`ETH $${price}`);
+    }, 120000);
   },
 };

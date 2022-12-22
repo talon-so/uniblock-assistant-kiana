fda518b92c685f3d2d62d1b1c754bced4ec3c934
8sunyuan <michaelsun97@gmail.com>
2022-12-22 10:30:19 -0500

    ETH price status, update every 1 minute interval

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,20 @@
 const axios = require('axios');
+const { ActivityType } = require("discord.js");
 
 module.exports = {
   name: "ready",
   once: true,
-  run(client) {
-    // console.log("Successfully logged in as " + client.user.tag);
-    // Update price status every 2 minutes
-    // setInterval(async () => {
-    //   const axiosRes = await axios.get(process.env.UNIBLOCK_BASE_URL + '/price?address=0x0000000000000000000000000000000000000000&chainId=1');
-    //   const price = axiosRes["priceData"][0]["price"];
-    //   client.user.setActivity(`ETH $${price}`);
-    // }, 120000);
+  async run(client) {
+    console.log("Successfully logged in as " + client.user.tag);
+
+    // Periodically update ETH price status every minute
+    setInterval(async () => {
+      console.log("Updating Ξ Price status" + client.user.tag);
+      const axiosRes = await axios.get(process.env.UNIBLOCK_BASE_URL + '/price?address=0x0000000000000000000000000000000000000000&chainId=1');
+      const price = axiosRes.data.priceData[0].price
+      client.user.setActivity(`Ξ ETH $${price}`, {
+        type: ActivityType.Watching
+      });
+    }, 60000);
   },
 };
